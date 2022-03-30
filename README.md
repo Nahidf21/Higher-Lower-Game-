2da93319c3ce4852b4c9600cb7ea5023537f1cba
Nahid Ferdous <nahid.mferdous@gmail.com>
2022-03-29 22:52:36 -0500

    Create game data
    create game data using list and dictionary

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Higher-Lower-Game-
+ckeck the link first:
+try to learn about the game first then code the game.
---
+++ game_data.py
@@ -0,0 +1,38 @@
+data = [
+    {
+        "name": "Instagram",
+        "follower_count": "346",
+        "description": "Social media platfrom",
+        "Country": "United States"
+    },
+    {
+        "name": "Facebook",
+        "follower_count": "750",
+        "description": "Social Media Platfrom",
+        "Country": "United States"
+    },
+    {
+        "name": "Tweet",
+        "follower_count": "980",
+        "description": "Social Media Platfrom",
+        "Country": "United States"
+    },
+    {
+        "name": "Tiktok",
+        "follower_count": "365",
+        "description": "Social Media Platfrom",
+        "Country": "China"
+    },
+    {
+        "name": "Lionel Messi",
+        "follower_count": "630",
+        "description": "Football player ",
+        "Country": "Argentine"
+    },
+    {
+        "name": "Cristiano Ronaldo",
+        "follower_count": "780",
+        "description": "Football Player",
+        "Country": "Portugal"
+    }
+]
