50cc5941dccdc2fb2d6bd343cd59d01f0970a057
Nahid Ferdous <nahid.mferdous@gmail.com>
2022-04-03 22:51:29 -0500

    split the  code concept

---
+++ main.py
@@ -0,0 +1,22 @@
+# Generate a random account from the game data.
+
+# Format account data into printable format.
+
+
+# Ask user for a guess.
+
+# Check if user is correct.
+## Get follower count.
+## If Statement
+
+# Feedback.
+
+
+# Score Keeping.
+
+# Make game repeatable.
+
+
+# Make B become the next A.
+
+# Clear screen between rounds.
