d2cf35c1b00aff522cce4c037370efa59449dd1e
Nahid Ferdous <nahid.mferdous@gmail.com>
2022-04-03 23:20:23 -0500

    create formate_data function

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-# Generate a random account from the game data.
+from game_data import data
+import random
 
-# Format account data into printable format.
+def format_data(account):
+    
+    """Format account data into printable format""" #docs string 
+    account_name=account["name"]
+    account_descr=account["description"]
+    account_country=account["country"]
+    return (f"{account_name}, is a {account_descr}, from {account_country}")
+
+
+
+# Generate a random account from the game data.
+account_a=random.choice(data)   #here  we get a dictionary for the list
+account_b=random.choice(data)   #here  we get a dictionary for the list
+if account_a == account_b:      #we need two separate dictionary thats why we use this formula
+    account_b=random.choice(data)
+
+
+print(f" Compare A: {format_data(account_a)}.")
+print(f" Compare B: {format_data(account_b)}.")
 
 
 # Ask user for a guess.
